962d3e7d4909f944cd93d7e6124c52a46a84a8a9
ernazar <ernazarnurbekuulu15@gmail.com>
2023-11-20 10:28:34 +0600

    two sums with object solved

--- leetcode-1.js
+++ leetcode-1.js
@@ -1,5 +1,32 @@
+const twoSumsWithMap = (arr, target) => {
+  const memory = new Map()
 
-const twoSums = (arr)=>{
-	
+  for (let i = 0; i < arr.length; i++) {
+    const numberToFind = target - arr[i]
+    if (!memory.has(numberToFind)) {
+      return memory.set(numberToFind, i)
+    }
+    return [memory.get(numberToFind), i]
+  }
+
+  return null
 }
 
+let res = twoSumsWithMap([1, 2, 4, 6, 7], 9)
+console.log({ res })
+
+// asdadasdas
+
+const twoSumsWithObj = (arr, target) => {
+  const memory = {}
+
+  for (let i = 0; i < arr.length; i++) {
+    const numberToFind = target - arr[i]
+    if (memory[arr[i]] !== undefined) {
+      return [memory[arr[i]], i]
+    }
+    memory[numberToFind] = i
+  }
+}
+
+const resObj = twoSumsWithObj([1, 2, 4, 6, 7], 13)
