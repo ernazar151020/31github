dfae5f1be24ea1fc208fe866390d63e8f279e2a7
ernazar <ernazarnurbekuulu15@gmail.com>
2023-11-23 12:33:17 +0600

    code optimized

--- leetcode-4.js
+++ leetcode-4.js
@@ -24,3 +24,44 @@ const modifyString = string => {
 const res = areStringsEqual('ab#z', 'az#z')
 
 console.log('res', res)
+
+const optimizedSolution = (s, t) => {
+  let p1 = s.length - 1
+  let p2 = t.length - 1
+  while (p1 >= 0 || p2 >= 0) {
+    if (s[p1] === '#' || t[p2] === '#') {
+      if (s[p1] === '#') {
+        let backcount = 2
+        while (backcount > 0) {
+          p1--
+          backcount--
+          if (s[p1] === '#') {
+            backcount = backcount + 2
+          }
+        }
+      }
+      if (t[p2] === '#') {
+        let backcount = 2
+        while (backcount > 0) {
+          p2--
+          backcount--
+          if (t[p2] === '#') {
+            backcount = backcount + 2
+          }
+        }
+      } else {
+        if (s[p1] !== t[p2]) {
+          return false
+        } else {
+          p1--
+          p2--
+        }
+      }
+    }
+    return true
+  }
+}
+
+const ress = optimizedSolution('ab#z', 'az#z')
+
+console.log({ ress })
