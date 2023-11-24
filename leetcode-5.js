784cb93d4a22c04c4298686a8d854a4ccd57b407
ernazar <ernazarnurbekuulu15@gmail.com>
2023-11-24 09:47:04 +0600

    code optimized

--- leetcode-5.js
+++ leetcode-5.js
@@ -19,3 +19,26 @@ const longestSubstr = str => {
 
 const res = longestSubstr('abcbdca')
 console.log(res)
+
+const optimalSolution = str => {
+  if (str.length <= 1) return str.length
+  let left = 0
+  let memory = {}
+  let longest = 0
+  for (let right = 0; right < str.length; right++) {
+    const currentChar = str[right]
+    const prevSeen = memory[currentChar]
+    if (prevSeen >= left) {
+      left = prevSeen + 1
+    }
+    memory[currentChar] = right
+    longest = Math.max(longest, right - left + 1)
+  }
+
+  console.log({ memory })
+
+  return longest
+}
+
+const res1 = optimalSolution('abcbdca')
+console.log(res1)
