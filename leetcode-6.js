28a0ca9f6550a7dd3838f26bb3878b550b53a142
ernazar <ernazarnurbekuulu15@gmail.com>
2023-11-25 12:55:02 +0600

    polindrome is solved

--- leetcode-6.js
+++ leetcode-6.js
@@ -1,3 +1,35 @@
+const polindrom = str => {
+  const reversed = str.split('').reverse('')
 
+  if (str === reversed.join('')) {
+    return true
+  }
 
+  return false
+}
 
+const res = polindrom('abac')
+
+console.log({ res })
+
+// ***********IS VALID PALINDROME ****************
+
+const isValidPolindrome = str => {
+  str = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase()
+
+  let left = 0
+  let right = str.length - 1
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false
+    }
+    left++
+    right--
+  }
+
+  return true
+}
+
+const res1 = isValidPolindrome('Am an , a plan , a canal:Panama')
+
+console.log({ res1 })
