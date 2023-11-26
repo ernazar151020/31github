b389cc96d78daee809b69626f77b5049458f15ee
ernazar <ernazarnurbekuulu15@gmail.com>
2023-11-26 08:34:23 +0600

    isAlmost ploindrome question is going to solved

---
+++ leetcode-7.js
@@ -0,0 +1,7 @@
+// If i remove one letter from string and it becomes a polindrome so i should retuen true , otherwise false
+
+const isAlmostPolindrome = str => {}
+
+const res = isAlmostPolindrome('abccdba')
+
+console.log({ res })
