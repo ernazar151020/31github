c309a0ba785552c3d05487717b901a81c326baa9
ernazar <ernazarnurbekuulu15@gmail.com>
2023-11-28 10:21:08 +0600

    .

--- leetcode-10.js
+++ leetcode-10.js
@@ -1,3 +1,15 @@
+const flattenLinkedList = head => {
+  if (!head) return head
+  let currentNode = head
 
-
-const flattenLinkedList = ()=>{}
+  while (currentNode !== null) {
+    if (currentNode.child === null) {
+      currentNode = currentNode.next
+    } else {
+      let tail = currentNode.child
+      while (tail.next !== null) {
+        tail = tail.next
+      }
+    }
+  }
+}
