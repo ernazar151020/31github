733851c3a8e804cec203ec499b804f9e1b0f7c9a
ernazar <ernazarnurbekuulu15@gmail.com>
2023-11-29 08:42:58 +0600

    tortoise and hair linked list algorithm solved

--- leetcode-11.js
+++ leetcode-11.js
@@ -11,3 +11,30 @@ const findCycleInLinkedList = head => {
 
   return currentNode
 }
+
+// Floyds Tortoise and Hare Algortihm
+
+const hareTortise = head => {
+  let hare = head
+  let tortoise = head
+  while (true) {
+    hare = hare.next
+    tortoise = tortoise.next
+    if (hare === null || hare.next === null) {
+      return false
+    } else {
+      hare = hare.next
+    }
+    if (tortoise === hare) break
+  }
+
+  let p1 = head
+  let p2 = tortoise
+
+  while (p1 !== p2) {
+    p1 = p1.next
+    p2 = p2.next
+  }
+
+  return p1
+}
