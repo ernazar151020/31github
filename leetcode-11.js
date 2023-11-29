17f038daaf7b7fad72b9e3163f0c41f17d4af5e5
ernazar <ernazarnurbekuulu15@gmail.com>
2023-11-29 08:31:18 +0600

    cycle detection in linked list

--- leetcode-11.js
+++ leetcode-11.js
@@ -1,1 +1,13 @@
-const hasLinkedListCycle = head => {}
+const findCycleInLinkedList = head => {
+  let currentNode = head
+  const seenNodes = new Set()
+  while (!seenNodes.has(currentNode)) {
+    if (currentNode.next === null) {
+      return false
+    }
+    seenNodes.add(currentNode)
+    currentNode = currentNode.next
+  }
+
+  return currentNode
+}
