2204c64e553a4c2fd605e75f964b7c08354a8a1a
ernazar <ernazarnurbekuulu15@gmail.com>
2023-11-30 11:22:03 +0600

    .

--- leetcode-12.js
+++ leetcode-12.js
@@ -1,11 +1,23 @@
-const brackers = {
+const brackets = {
   '{': '}',
   '[': ']',
-  '{': '}'
+  '(': ')'
 }
 
-const validBrackets = str => {}
+const validBrackets = str => {
+  const memory = []
 
-const res = validBrackets('{()}}')
+  for (let i = 0; i < str.length; i++) {
+    if (brackets[str[i]]) {
+      memory.push(str[i])
+    } else {
+      const leftBracket = memory.pop()
+      const correctBracket = brackets[leftBracket]
+      if (str[i] !== correctBracket) return false
+    }
+  }
+}
+
+const res = validBrackets('{()}')
 
 console.log({ res })
