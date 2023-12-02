401aaed32c0fa80ece38903adb39f944b6ef07ac
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-02 12:24:23 +0600

    .

--- leetcode-14.js
+++ leetcode-14.js
@@ -2,3 +2,36 @@
 
 // Que is a first in first out
 // Stack is a first in last out
+
+class Que {
+  constructor () {
+    this.in = []
+    this.out = []
+  }
+
+  enque (val) {
+    this.in.push(val)
+  }
+  deque () {
+    if (this.out.length === 0) {
+      while (this.in.length) {
+        this.out.push(this.in.pop())
+      }
+    }
+    return this.out.pop()
+  }
+
+  peak () {
+    if (this.out.length === 0) {
+      while (this.in.length) {
+        this.out.push(this.in.pop())
+      }
+    }
+
+    return this.out[this.out.length - 1]
+  }
+
+  empty () {
+    return this.length === 0 && this.in.length === 0 && this.out.length == 0
+  }
+}
