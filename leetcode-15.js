7488b7f6b88225944a73479acbd344184372d577
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-03 10:56:16 +0600

    .

--- leetcode-15.js
+++ leetcode-15.js
@@ -1,3 +1,18 @@
 // Tail recursion
 // Normal recursion space O(n)
 // Tail recursion space O(1)
+
+// Noram recursion
+
+function recFuctorial (x) {
+  if (x <= 1) {
+    return 1
+  } else {
+    console.log(x)
+    return x * recFuctorial(x - 1)
+  }
+}
+
+const res = recFuctorial(5)
+
+console.log({ res })
