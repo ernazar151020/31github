9577106bcca7d540f13832c7862a1b3327595ce3
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-04 11:30:33 +0600

    .

--- leetcode-16.js
+++ leetcode-16.js
@@ -1,1 +1,11 @@
 // Sorting with recursion
+
+// Quick sort sorts array in place , does not return new array
+
+// Sort element 
+
+const quickSort = () => {}
+
+const res = quickSort()
+
+console.log(res)
