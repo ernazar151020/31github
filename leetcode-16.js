0ac6c71f2c4fb61b9be0b8062203cdf6c71b2269
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-04 11:42:21 +0600

    divide and conquer

--- leetcode-16.js
+++ leetcode-16.js
@@ -4,7 +4,11 @@
 
 // Sort element
 
-// Divide and Conquer paradigm
+// Divide and Conquer is algorithmic paradigm
+// 1.Multi-branched recursion
+// 2.Breaks a problem into multiple smaller but same sub problems
+// 3.Combines the solutions of sub-problems into the solution for the original problem
+
 const quickSort = () => {}
 
 const res = quickSort()
