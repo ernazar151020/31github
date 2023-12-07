86c7a16d17b9303a9bad22bc8d6ea69bdc912de4
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-07 09:44:51 +0600

    .

--- leetcode-18.js
+++ leetcode-18.js
@@ -3,9 +3,21 @@
 // Time complexity of Binary search is O(logn)
 
 const binarySearch = (array, target) => {
-  const middle = array[array.length / 2]
+  let left = 0
+  let right = array.length - 1
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2)
+    const foundValue = array[middle]
+    if (foundValue === target) {
+      return middle
+    } else if (foundValue < target) {
+      left = middle + 1
+    } else {
+      right = mid - 1
+    }
+  }
 
-  console.log({ middle })
+  return -1
 }
 
 const res = binarySearch([1, 2, 3, 4, 5, 6, 7, 8], 8)
