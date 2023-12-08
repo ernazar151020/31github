c1e36426c1092c32b76f8bd5bbf99af74541302b
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-08 11:38:41 +0600

    .

--- leetcode-19.js
+++ leetcode-19.js
@@ -2,3 +2,28 @@
 // Your solution should run in O(logN) time
 
 // [1,1,1,2,2,3,4,5,5,5,6,7]  => target=5 , [7,9] 5 тин башталганы жана аягын табабыз
+
+const binarySearch = (array, target) => {
+  let start = 0
+  let end = array.length - 1
+
+  const middle = Math.floor((start + end) / 2)
+
+  while (start <= end) {
+    let foundValue = array[middle]
+    if (foundValue === target) {
+      return middle
+    } else if (foundValue < target) {
+      start = middle + 1
+    } else {
+      end = middle - 1
+    }
+  }
+  return -1
+}
+
+const searchRange = (nums, target) => {}
+
+const res = searchRange([1, 1, 2, 3, 4, 5, 5, 5, 6, 6], 5)
+
+console.log(res)
