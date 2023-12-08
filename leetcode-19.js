d961e3590baeccc919387f873d4ea93796a0af33
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-08 11:21:12 +0600

    .

--- leetcode-19.js
+++ leetcode-19.js
@@ -1,2 +1,4 @@
 // Given an array of integesr sorted in ascending order , return the starting and ending index of a given target value in an array [x,y]
 // Your solution should run in O(logN) time
+
+// [1,1,1,2,2,3,4,5,5,5,6,7]  => target=5 , [7,9] 5 тин башталганы жана аягын табабыз
