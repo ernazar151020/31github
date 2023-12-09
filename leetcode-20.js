c7125820e32fb7129f1e45ac36f4bca75a53a0b6
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-09 08:18:52 +0600

    .

--- leetcode-20.js
+++ leetcode-20.js
@@ -1,7 +1,9 @@
 // Binary Trees and Binary Search Trees
 // Topics in Binary Tree
+// ************SEARCH TYPE*************
 // 1) Breadth First Search
 // 1) Depth First Search
+// *********TRAVERSAL TYPE**************
 // 1) Pre-order traversal
 // 1) In-order traversal
 // 1) Post-order traversal
