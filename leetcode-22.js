12537631f261b40e596fb337fa3decb9e0df11b4
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-10 18:11:43 +0600

    .

--- leetcode-22.js
+++ leetcode-22.js
@@ -1,4 +1,32 @@
 // Optimized solution for previous example
-const getGreatestAmountOptimized = nums => {}
+const getGreatestAmountOptimized = nums => {
+  let maxArea = 0
+
+  let left = 0
+  let right = nums.length - 1
+
+  while (left < right) {
+    let step = right - left
+    let area = Math.min(nums[left], nums[right]) * step
+    console.log({
+      left: nums[left],
+      right: nums[right],
+      step,
+      area
+    })
+
+    maxArea = Math.max(maxArea, area)
+
+    if (nums[left] > nums[right]) {
+      right--
+    } else {
+      left++
+    }
+  }
+
+  return maxArea
+}
 
 const res = getGreatestAmountOptimized([1, 8, 6, 2, 9, 4])
+
+console.log(res)
