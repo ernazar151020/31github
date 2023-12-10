fd1155144f4b90cf34aa0ef5267c8da0bffd8809
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-10 17:19:18 +0600

    .

--- leetcode-21.js
+++ leetcode-21.js
@@ -1,2 +1,3 @@
 // Array Question . Resolving Array questions
 
+// You are given an array of positive integers where each integer represents the height of a vertical line on a chart . Find two lines which together with the x-axis forms a container that would hold the greatest amount of water . Return the ares of water it woild hold.
