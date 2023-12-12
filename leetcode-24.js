9b28f605445e14ee371660c4ed07d72c5d751c07
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-12 09:07:33 +0600

    .

--- leetcode-24.js
+++ leetcode-24.js
@@ -1,6 +1,21 @@
 // merge continuously repeted elements
 
-const mergeRepeatedElements = str => {}
+const mergeRepeatedElements = str => {
+  let currentValue = str[0]
+  let count = 1
+  const result = ''
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === currentValue) {
+      count++
+    } else {
+      currentValue = currentValue[i]
+      result += currentValue + count > 1 ? count : ''
+      count = 1
+    }
+  }
+
+  return result
+}
 
 const res = mergeRepeatedElements('aaabbbcdd')
 
