dd8bfa765d687cef0f286565f103ec4dbbc76e83
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-14 08:30:20 +0600

    .

--- leetcode-26.js
+++ leetcode-26.js
@@ -1,22 +1,25 @@
 // Find longest not repeated substring
 
 const longestSubstring = str => {
+  console.log(str.length)
   if (str.length <= 1) return str.length
 
   let longest = 0
-  const seenChars = {}
-  for (let right = 0; right < str.length; right++) {
-    for (let left = right; left < str.length; left++) {
-      let currentLength = 0
-      if (!seenChars[left]) {
+
+  for (let left = 0; left < str.length; left++) {
+    const seenChars = {}
+    let currentLength = 0
+    for (let right = left; right < str.length; right++) {
+      if (!seenChars[str[right]]) {
         currentLength++
-        seenChars[left] = true
+        seenChars[str[right]] = true
         longest = Math.max(longest, currentLength)
       } else {
         break
       }
     }
   }
+  return longest
 }
 
 const res = longestSubstring('abccabb')
