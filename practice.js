cb6fb3a6d7c31b28fe40d3bb83c23913a2678c05
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-15 10:44:36 +0600

    .

---
+++ practice.js
@@ -0,0 +1,7 @@
+// Given array of integers  , find two contigious integers thatforms the geatest sum
+
+const greatestSum = nums => {}
+
+const res = greatestSum([1, 3, 7, 9, 2, 4])
+
+console.log({ res })
