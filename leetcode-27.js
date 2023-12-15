f4799171cf7009078655a7f63b3ea2764c12ec81
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-15 10:32:16 +0600

    .

--- leetcode-27.js
+++ leetcode-27.js
@@ -1,4 +1,19 @@
-const longestSubstring = str => {}
+const longestSubstring = str => {
+  let longest = 0
+  for (let left = 0; left < str.length; left++) {
+    const seenChars = {}
+    let currentCount = 0
+    for (let right = left; right < str.length; right++) {
+      if (!seenChars[str[right]]) {
+        seenChars[str[right]]
+        currentCount++
+        longest = Math.max(longest, currentCount)
+      } else {
+        break
+      }
+    }
+  }
+}
 
 const res = longestSubstring('abcddaacc')
 
