bfe7a4d61949ea3fa343f05436f301e2f289765b
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-16 13:13:25 +0600

    .

--- leetcode-28.js
+++ leetcode-28.js
@@ -1,1 +1,8 @@
 // Sliding window technique
+// Optimize solution for longest substring
+
+const optimizedLongestSubstring = str => {}
+
+const res = optimizedLongestSubstring('abccabb')
+
+console.log(res)
