5812c9b928342aba4bc62025f884fa2fdbd828ef
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-17 06:37:06 +0600

    .

--- leetcode-29.js
+++ leetcode-29.js
@@ -2,7 +2,13 @@
 // Mostly will be given as a subpriblem
 
 // 1
-const aPolindrome = str => {}
+const aPolindrome = str => {
+  const toArr = str.split('').reverse().join('')
+  if (toArr === str) {
+    return true
+  }
+  return false
+}
 
 const res = aPolindrome('ererere')
 
