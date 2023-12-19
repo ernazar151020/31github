bcc168c7efeb02c836bafbe2afecb1c5d6248f92
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-19 09:10:39 +0600

    .

--- leetcode-30.js
+++ leetcode-30.js
@@ -3,4 +3,4 @@
 // Subproblems
 // Palindromes
 
-
+// Given a string , determine if it is almost a polindrome . A string is almost a polindrome if it becomes a polindrome by removing 1 letter . Consider only alphanumeric characters and ignore case sensitivity
