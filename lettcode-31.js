5a603ec1a731c539a28f0df2776b6a77299b39b6
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-20 08:52:34 +0600

    .

--- lettcode-31.js
+++ lettcode-31.js
@@ -2,7 +2,21 @@
 
 // You must buy exactly two chocolates in such a way that you still have some non-negative leftover money. You would like to minimize the sum of the prices of the two chocolates you buy.
 
-const buyChoco = (prices, money) => {}
+const buyChoco = (prices, money) => {
+  let sum = 0
+  for (let i = 0; i < prices.length; i++) {
+    const currentSum = prices[i] + prices[i + 1]
+    if (currentSum < money) {
+      if (sum === 0) {
+        sum = currentSum
+      } else {
+        sum = Math.min(sum, currentSum)
+      }
+    }
+  }
+
+  console.log({ sum })
+}
 
 const res = buyChoco([1, 2, 2], 3)
 const res2 = buyChoco([98, 54, 6, 34, 66, 63, 52, 39], 62) // 22 Output
