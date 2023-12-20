99d8a1e266a2fa5f7a2fc494fcc2fca224f9965e
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-20 08:47:36 +0600

    .

---
+++ lettcode-31.js
@@ -0,0 +1,10 @@
+// You are given an integer array prices representing the prices of various chocolates in a store. You are also given a single integer money, which represents your initial amount of money.
+
+// You must buy exactly two chocolates in such a way that you still have some non-negative leftover money. You would like to minimize the sum of the prices of the two chocolates you buy.
+
+const buyChoco = (prices, money) => {}
+
+const res = buyChoco([1, 2, 2], 3)
+const res2 = buyChoco([98, 54, 6, 34, 66, 63, 52, 39], 62) // 22 Output
+
+console.log({ res, res2 })
