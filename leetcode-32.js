7dedcd199ec0583a3aeed82c20a822d2b25bd8f2
ernazar <ernazarnurbekuulu15@gmail.com>
2023-12-21 09:18:08 +0600

    .

--- leetcode-32.js
+++ leetcode-32.js
@@ -3,7 +3,14 @@
 // palindrome
 // , and false otherwise.
 
-const isPalindrome = str => {}
+const isPalindrome = str => {
+  const toString = str.toString()
+  const reversed = toString.split('').reverse()
+  if (toString === reversed) {
+    return true
+  }
+  return false
+}
 
 const res = isPalindrome(121)
 
